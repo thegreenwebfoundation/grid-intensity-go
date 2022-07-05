b9b3a2d8d0083c352d4a02e24f1a07726a24daaf
Ross Fairbanks <ross@rossfairbanks.com>
2022-07-05 13:35:15 +0200

    Save country code to config file

--- cmd/root.go
+++ cmd/root.go
@@ -1,19 +1,25 @@
 package cmd
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
+	"io/fs"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/Xuanwo/go-locale"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 
 	"github.com/thegreenwebfoundation/grid-intensity-go/ember"
 )
 
 const (
-	countryCode = "country-code"
+	configFileLocation = ".config/grid-intensity/config.yaml"
+	countryCode        = "country-code"
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -29,12 +35,7 @@ grid is greener or at locations where carbon intensity is lower.
 	grid-intensity -c BOL`,
 
 	Run: func(cmd *cobra.Command, args []string) {
-		country, err := cmd.Flags().GetString(countryCode)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		err = getGridIntensityForCountry(country)
+		err := runWithError()
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -50,20 +51,10 @@ func Execute() {
 
 func init() {
 	rootCmd.Flags().StringP(countryCode, "c", "", "Country code in ISO 3 character format")
+	viper.BindPFlag(countryCode, rootCmd.Flags().Lookup(countryCode))
 }
 
 func getGridIntensityForCountry(countryCode string) error {
-	if countryCode == "" {
-		// No country code provided so try to detect it from the user's locale.
-		tag, err := locale.Detect()
-		if err != nil {
-			return err
-		}
-
-		region, _ := tag.Region()
-		countryCode = region.ISO3()
-	}
-
 	result, err := ember.GetGridIntensityForCountry(countryCode)
 	if err != nil {
 		return err
@@ -75,5 +66,77 @@ func getGridIntensityForCountry(countryCode string) error {
 	}
 
 	fmt.Println(string(bytes))
+	return nil
+}
+
+// getCountryCode prompts the user to enter a country code. We try to detect
+// a country code from the user's locale but the user can enter another value.
+func getCountryCode() (string, error) {
+	tag, err := locale.Detect()
+	if err != nil {
+		return "", err
+	}
+
+	region, _ := tag.Region()
+	country := region.ISO3()
+
+	fmt.Printf("Provider %s needs an ISO country code as a region parameter.\n", ember.Provider)
+	if country != "" {
+		fmt.Printf("%s detected from your locale.\n", country)
+	}
+
+	var reader = bufio.NewReader(os.Stdin)
+	country, err = reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+
+	return strings.TrimSpace(country), nil
+}
+
+func runWithError() error {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return nil
+	}
+
+	configFile := filepath.Join(homeDir, configFileLocation)
+	viper.SetConfigFile(configFile)
+
+	err = viper.ReadInConfig()
+	if _, ok := err.(*fs.PathError); ok {
+		// Create config file if it doesn't exist.
+		_, err = os.Create(configFile)
+		if err != nil {
+			return err
+		}
+	} else if err != nil {
+		return err
+	}
+
+	country := viper.GetString(countryCode)
+	if err != nil {
+		return err
+	}
+
+	if country == "" {
+		country, err = getCountryCode()
+		if err != nil {
+			return err
+		}
+
+		viper.Set(countryCode, country)
+	}
+
+	err = getGridIntensityForCountry(country)
+	if err != nil {
+		return err
+	}
+
+	err = viper.WriteConfig()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
