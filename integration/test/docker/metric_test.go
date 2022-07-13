ec08eb8b9cf5cedb0e95372784ab8f6cd5f28324
Ross Fairbanks <ross@rossfairbanks.com>
2022-07-13 17:37:48 +0200

    Fix metric name in test

--- integration/test/docker/metric_test.go
+++ integration/test/docker/metric_test.go
@@ -48,9 +48,9 @@ func Test_GridIntensityMetric(t *testing.T) {
 		t.Fatalf("expected nil got %v", err)
 	}
 
-	expectedMetricText := "grid_intensity_carbon_actual{provider=\"ember-climate.org\",region=\"GBR\",units=\"gCO2 per kWh\"}"
+	expectedMetricText := "grid_intensity_carbon_average{provider=\"ember-climate.org\",region=\"GBR\",units=\"gCO2 per kWh\"}"
 
 	if !strings.Contains(metrics, expectedMetricText) {
-		t.Fatalf("expected metric text %q not found", expectedMetricText)
+		t.Fatalf("expected metric text %q not found got %q", expectedMetricText, metrics)
 	}
 }
